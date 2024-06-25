53ec025bb95f7aa7ed07ad2cf1ed23cf57ac7a90
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-06-25 14:17:49 +0900

    Initial Commit

---
+++ MVVMExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVMExample
+//
+//  Created by Brandon Jones on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVMExample/MVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExampleApp.swift
+//  MVVMExample
+//
+//  Created by Brandon Jones on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
